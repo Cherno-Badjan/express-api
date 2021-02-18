c14b48b5ff896a5522dd96f86bc63851124f934f
Cherno Badjan <badjancherno@gmail.com>
2021-02-18 14:33:20 -0800

    data

---
+++ data.js
@@ -0,0 +1,76 @@
+const data = [{
+    id: 1,
+    first_name: 'Netty',
+    last_name: 'Lerven',
+    Age: 35,
+    gender: 'Non-binary',
+    vegetarian: false,
+}, {
+    id: 2,
+    first_name: 'Assu',
+    last_name: 'Baba',
+    Age: 25,
+    gender: 'Male',
+    vegetarian: false,
+}, {
+    id: 3,
+    first_name: 'Paul',
+    last_name: 'Bogle',
+    Age: 30,
+    gender: 'Male',
+    vegetarian: false,
+}, {
+    id: 4,
+    first_name: 'Miss',
+    last_name: 'Njie',
+    Age: 28,
+    gender: 'Female',
+    vegetarian: true,
+}, {
+    id: 5,
+    first_name: 'Sirius',
+    last_name: 'Black',
+    Age: 35,
+    gender: 'Non-binary',
+    vegetarian: false,
+}, {
+    id: 6,
+    first_name: 'James',
+    last_name: 'Potter',
+    Age: 35,
+    gender: 'Male',
+    vegetarian: false,
+}, {
+    id: 7,
+    first_name: 'Lucius',
+    last_name: 'Malfoy',
+    Age: 40,
+    gender: 'Non-binary',
+    vegetarian: true,
+}, {
+    id: 8,
+    first_name: 'Nearly Headless',
+    last_name: 'Nick',
+    Age: 1500,
+    gender: 'Non-binary',
+    vegetarian: false,
+}, {
+    id: 9,
+    first_name: 'Bloody',
+    last_name: 'Baron',
+    Age: 2000,
+    gender: 'Male',
+    vegetarian: true,
+}, {
+    id: 10,
+    first_name: 'Odell',
+    last_name: 'Beckham',
+    Age: 26,
+    gender: 'Male',
+    vegetarian: false,
+}
+]
+
+module.exports = {
+    data
+};
